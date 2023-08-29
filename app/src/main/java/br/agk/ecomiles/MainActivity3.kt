1828f51df5777d78b734ec85a71affb56baeb65e
Abigail Lima <abigailmvlima@outlool.com>
2023-08-29 17:40:14 -0300

    inclui lista

--- app/src/main/java/br/agk/ecomiles/MainActivity3.kt
+++ app/src/main/java/br/agk/ecomiles/MainActivity3.kt
@@ -17,9 +17,9 @@ class MainActivity3 : AppCompatActivity() {
 
         // Dados de exemplo para a lista
         val dataList = listOf(
-            Triple("Título 1", "Descrição 1","https://m.media-amazon.com/images/I/71IMo1bzDRL._AC_SX679_.jpg"),
-            Triple("Título 2", "Descrição 2","https://images-na.ssl-images-amazon.com/images/G/32/kindle/journeys/PUx_1hPmoyVDyBdI/ZjUyYzMwYTEt-w372._SY232_CB598124054_.jpg"),
-            Triple("Título 3", "Descrição 3","https://images-na.ssl-images-amazon.com/images/G/32/br-events/2022/gateway/CategoryBubbleShoveler/XCM_CUTTLE_ORIGIN_1442798_2419248_400x400_2X_pt_BR_Version_1._CB633595703_.png")
+            Quadruple("Novo Kindle 11ª Geração", "Marca: Amazon","https://m.media-amazon.com/images/I/71IMo1bzDRL._AC_SX679_.jpg", "1.000 milhas"),
+            Quadruple("Título 2", "Descrição 2","https://images-na.ssl-images-amazon.com/images/G/32/kindle/journeys/PUx_1hPmoyVDyBdI/ZjUyYzMwYTEt-w372._SY232_CB598124054_.jpg", "500 milhas"),
+            Quadruple("Título 3", "Descrição 3","https://images-na.ssl-images-amazon.com/images/G/32/br-events/2022/gateway/CategoryBubbleShoveler/XCM_CUTTLE_ORIGIN_1442798_2419248_400x400_2X_pt_BR_Version_1._CB633595703_.png", "1.000 milhas")
         )
 
         // Adiciona itens à lista dinamicamente
@@ -28,9 +28,11 @@ class MainActivity3 : AppCompatActivity() {
             val titleTextView = itemView.findViewById<TextView>(R.id.titleTextView)
             val descTextView = itemView.findViewById<TextView>(R.id.descTextView)
             val itemImageView = itemView.findViewById<ImageView>(R.id.itemImageView)
+            val milhasTextView = itemView.findViewById<TextView>(R.id.milhasTextView)
 
             titleTextView.text = item.first
             descTextView.text = item.second
+            milhasTextView.text = item.fourth
 
             // Carregar a imagem usando a biblioteca Glide (é necessário adicionar a dependência no build.gradle)
             Glide.with(this)
---
+++ app/src/main/java/br/agk/ecomiles/Quadruple.kt
@@ -0,0 +1,9 @@
+package br.agk.ecomiles
+
+data class Quadruple<A, B, C, D>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D
+)
+
