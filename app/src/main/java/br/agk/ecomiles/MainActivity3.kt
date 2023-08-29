bbaf5fbb987829803178617325c1977a9865c11a
elio.lima <macstudio@local>
2023-08-29 11:18:57 -0300

    ajuste header

--- app/src/main/java/br/agk/ecomiles/MainActivity3.kt
+++ app/src/main/java/br/agk/ecomiles/MainActivity3.kt
@@ -10,6 +10,7 @@ import com.bumptech.glide.Glide
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main3)
 
         val listContainer = findViewById<LinearLayout>(R.id.listContainer)
